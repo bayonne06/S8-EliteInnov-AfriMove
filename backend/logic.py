15f2e29f415f0d5e04aedfb781798e4a122ec639
grace-moussoni <gracemoussoni2@gmail.com>
2026-08-01 16:15:26 +0200

    fix: résolution du bug dans le tri qui retourner le compte correspondant au numero de telephone

--- backend/logic.py
+++ backend/logic.py
@@ -539,4 +539,8 @@ def trouver_compte_par_telephone(comptes, telephone):
         -> {"id": 1, "telephone": "066123456", "nom": "Franck"}
         trouver_compte_par_telephone(comptes, "055999999") -> None
     """
-    return trouver_compte_par_telephone(comptes, telephone) is None
+    for compte in comptes:
+        if compte["telephone"] == telephone:
+            return compte
+
+    return None
